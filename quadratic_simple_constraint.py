fd5e15572804c1ea8d5adb3f3601f496463aea27
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-10-19 09:17:34 +0200

    [refactor] Modify constrained simple based on the Optuna code convention

--- quadratic_simple_constraint.py
+++ quadratic_simple_constraint.py
@@ -1,16 +1,20 @@
 """
 Optuna example that optimizes a simple quadratic function with constraints.
 
-In this example, we optimize a simple quadratic function with constraints.
+This setup is called `constrained optimization`.
+Constrained optimization is useful when we would like to optimize some metrics under some constraints.
+For example, we would like to maximize the accuracy of a deep neural networks while guaranteeing that
+deep neural networks fit on your hardware, e.g. 8GB of memory consumption.
+In this case, constrained optimization aims to yield an optimal solution that satisfied such constraints.
 
-Please check https://optuna.readthedocs.io/en/stable/faq.html#id16 as well.
+Please also check https://optuna.readthedocs.io/en/stable/faq.html#id16 as well.
 
 """
 
 import optuna
 
 
-# Define a simple 2-dimensional objective function with constraints.
+# Define a simple 2-dimensional objective function to minimize.
 def objective(trial):
     x = trial.suggest_float("x", -100, 100)
     y = trial.suggest_categorical("y", [-1, 0, 1])
@@ -18,21 +22,18 @@ def objective(trial):
 
 
 # Define a function that returns constraints.
+# The constraints are to satisfy `c1 <= 0` and `c2 <= 0` simultaneously.
+# If we would like to make the constraint like `c1 <= a`, we simply need to return `c1 - a` instead of `c1`.
 def constraints(trial):
     params = trial.params
     x, y = params["x"], params["y"]
-    # c1 <= 0 and c2 <= 0 must be satisfied.
     c1 = 3 * x * y + 10
     c2 = x * y + 30
     return [c1, c2]
 
 
-if __name__ == "__main__":
-    # We minimize obj1 and maximize obj2.
-    sampler = optuna.samplers.TPESampler(constraints_func=constraints)
-    study = optuna.create_study(sampler=sampler)
-    study.optimize(objective, n_trials=500, timeout=1)
-
+def get_feasible_trial_ids(trials):
+    feasible_indices = []
     best_trial_id, best_value, best_params = None, float("inf"), None
     for t in study.trials:
         infeasible = any(c > 0.0 for c in t.system_attrs["constraints"])
@@ -43,7 +44,25 @@ if __name__ == "__main__":
             best_params = t.params.copy()
             best_trial_id = t._trial_id
 
-    if best_trial_id is None:
-        print("All trials violated the constraints.")
+
+if __name__ == "__main__":
+    sampler = optuna.samplers.TPESampler(constraints_func=constraints)
+    study = optuna.create_study(sampler=sampler)
+    study.optimize(objective, n_trials=500, timeout=1)
+
+    print("Number of finished trials: ", len(study.trials))
+
+    feasible_indices = [
+        trial.number
+        for trial in study.trials
+        if all(c <= 0.0 for c in trial.system_attrs["constraints"])
+    ]
+    if len(feasible_indices) == 0:
+        print("No trials satisfied all the constraints.")
     else:
-        print(f"Best value is {best_value} with params {best_params}")
+        best_trial_id = sorted(feasible_indices, key=lambda i: study.trials[i].value)[0]
+        best_trial = study.trials[best_trial_id]
+        print(f"Best trial was found at Trial#{best_trial_id}")
+        print(f"  Params: {best_trial.params}")
+        print(f"  Value: {best_trial.value}")
+        print(f"  Constraints: {best_trial.system_attrs['constraints']}")
