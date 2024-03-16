5de7ee4b82ad93dfddd842e36a24f3a5f1772069
Kento Nozawa <nzw0301@preferred.jp>
2024-03-17 01:11:19 +0900

    Support tensorflow 2.16.1 and separate CI run for tensorflow estimator

--- tensorflow/tensorflow_estimator_simple.py
+++ tensorflow/tensorflow_estimator_simple.py
@@ -13,10 +13,14 @@ import tempfile
 import urllib
 
 import optuna
+from packaging import version
 import tensorflow_datasets as tfds
 
 import tensorflow as tf
 
+
+if version.parse(tf.__version__) >= version.parse("2.16.0"):
+    raise RuntimeError("tensorflow<2.16.0 is required for this example.")
 
 # TODO(crcrpar): Remove the below three lines once everything is ok.
 # Register a global custom opener to avoid HTTP Error 403: Forbidden when downloading MNIST.
