6c1d5984ce7c9d09ce23cf09de46580ae6072bf6
SubhadityaMukherjee <msubhaditya@gmail.com>
2025-03-26 18:33:21 +0100

    applied required formatting

--- openml/openml_simple.py
+++ openml/openml_simple.py
@@ -3,7 +3,8 @@
 Hyperparameter Optimization Benchmark with OpenML
 ====================================================
 
-In this tutorial, we walk through how to conduct hyperparameter optimization experiments using OpenML and OptunaHub.
+In this tutorial, we walk through how to conduct hyperparameter optimization experiments using
+OpenML and OptunaHub.
 """
 
 ############################################################################
@@ -13,22 +14,27 @@ In this tutorial, we walk through how to conduct hyperparameter optimization exp
 
 # License: BSD 3-Clause
 
+import logging
+
+import optuna
+
 import openml
 from openml.extensions.sklearn import cat
 from openml.extensions.sklearn import cont
-import optuna
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
-import logging
+
 
 logger = logging.Logger(name="Experiment Logger", level=1)
 
-# Set your openml api key if you want to upload your results to OpenML (eg: https://openml.org/search?type=run&sort=date)
-# To get one, simply make an account (you don't need one for anything else, just to upload your results), go to your profile
-# and select the API-KEY. Or log in, and navigate to https://www.openml.org/auth/api-key
+# Set your openml api key if you want to upload your results to OpenML (eg:
+# https://openml.org/search?type=run&sort=date) . To get one, simply make an
+# account (you don't need one for anything else, just to upload your results),
+# go to your profile and select the API-KEY.
+# Or log in, and navigate to https://www.openml.org/auth/api-key
 openml.config.apikey = ""
 
 ############################################################################
@@ -36,13 +42,12 @@ openml.config.apikey = ""
 # ============================================
 
 # OpenML contains several key concepts which it needs to make machine learning research shareable.
-# A machine learning experiment consists of one or several runs, which describe the performance of an algorithm (called a flow in OpenML),
-# its hyperparameter settings (called a setup) on a task.
-# A Task is the combination of a dataset, a split and an evaluation metric
-# We choose a dataset from OpenML, (https://www.openml.org/d/1464) and a subsequent task (https://www.openml.org/t/10101)
-# To make your own dataset and task, please refer to
-# dataset - https://openml.github.io/openml-python/main/examples/30_extended/create_upload_tutorial.html#sphx-glr-examples-30-extended-create-upload-tutorial-py
-# task - https://openml.github.io/openml-python/main/examples/30_extended/tasks_tutorial.html#sphx-glr-examples-30-extended-tasks-tutorial-py
+# A machine learning experiment consists of one or several runs, which describe the performance of
+# an algorithm (called a flow in OpenML), its hyperparameter settings (called a setup) on a task.
+# A Task is the combination of a dataset, a split and an evaluation metric We choose a dataset from
+# OpenML, (https://www.openml.org/d/1464) and a subsequent task (https://www.openml.org/t/10101) To
+# make your own dataset and task, please refer to
+# https://openml.github.io/openml-python/main/examples/30_extended/create_upload_tutorial.html
 
 # https://www.openml.org/search?type=study&study_type=task&id=218
 task_id = 10101
@@ -59,13 +64,15 @@ preproc = ColumnTransformer([categorical_preproc, numerical_preproc])
 # Define a pipeline for the hyperparameter optimization (this is standark for Optuna)
 # =====================================================
 
-## Optuna explanation
+# Optuna explanation
 # we follow the `Optuna <https://github.com/optuna/optuna/>`__ search space design.
 
-## OpenML runs
-# We can simply pass the parametrized classifier to `run_model_on_task` to obtain the performance of the pipeline
+# OpenML runs
+# We can simply pass the parametrized classifier to `run_model_on_task` to obtain the performance
+# of the pipeline
 # on the specified OpenML task.
-# Do you want to share your results along with an easily reproducible pipeline, you can set an API key and just upload your results.
+# Do you want to share your results along with an easily reproducible pipeline, you can set an API
+# key and just upload your results.
 # You can find more examples on https://www.openml.org/
 
 
@@ -90,7 +97,7 @@ def objective(trial: optuna.Trial) -> Pipeline:
     else:
         logger.log(
             0,
-            "If you want to publish your results to OpenML, please set an apikey using `openml.config.apikey = ''`",
+            "If you want to publish your results to OpenML, please set an apikey",
         )
     accuracy = max(run.fold_evaluations["predictive_accuracy"][0].values())
     logger.log(0, f"Accuracy {accuracy}")
